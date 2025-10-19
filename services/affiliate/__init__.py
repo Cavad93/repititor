8f73b29a5b2ee46c9c6abfe56a33090ea3042eed
Cavad93 <cavad.93@yandex.ru>
2025-10-20 02:26:13 +0300

    Update __init__.py

--- services/affiliate/__init__.py
+++ services/affiliate/__init__.py
@@ -1,12 +1,8 @@
 # services/affiliate/__init__.py
 from .base import AffiliateServiceBase
 from .admitad import AdmitadService
-from .backit import BackitService
-from .yandex_market import YandexMarketService
 
 __all__ = [
     'AffiliateServiceBase',
-    'AdmitadService',
-    'BackitService',
-    'YandexMarketService'
+    'AdmitadService'
 ]
